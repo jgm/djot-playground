08bc40d34279b8a88a6f516fa37bd8b6d1373598
John MacFarlane <jgm@berkeley.edu>
2022-07-30 13:29:33 -0700

    Add style

--- src/PreviewIframeContent.ts
+++ src/PreviewIframeContent.ts
@@ -1,5 +1,14 @@
 export default `<!DOCTYPE html><html><head><meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+html {
+    line-height: 1.3;
+    font-family: ui-sans-serif;
+}
+</style>
+<script type="text/javascript" id="MathJax-script" async
+  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+</script>
 </head>
-<body id="htmlbody">(Loading...)</body>
+<body id="htmlbody" onchange="MathJax.typeset();">(Loading...)</body>
 </html>`
